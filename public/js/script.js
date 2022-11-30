c918013444b399addb438332b04df12505769d63
AAlkinani1986 <90129610+AAlkinani1986@users.noreply.github.com>
2022-11-30 17:53:09 +1100

    add commints to the script file

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,7 @@
-const cardList = [
+/* Array List contain cats objects such as title, picture 
+link , and description.
+*/
+const catList = [
   {
     title: 'Kitten 2',
     image: 'images/images-2.jpg',
@@ -12,7 +15,13 @@ const cardList = [
     description: 'Demo description about kitten 3',
   },
 ]
+/*
+Create a addCards Function will take items as an inputs
+will go through the items for each item will sign the 
+item objects such as image, title, link, and description into
+a variable itemToAppend with html code.
 
+*/
 const addCards = (items) => {
   items.forEach((item) => {
     let itemToAppend =
@@ -37,12 +46,38 @@ const addCards = (items) => {
     $('#card-section').append(itemToAppend)
   })
 }
+/*
+submitForm
+is a function that collect the inputs form popup window
+model firstName, lastName, password, and email.
+signing the value into the object fromData
+and show the data in the console log.
+
+*/
+const submitForm = () => {
+  let formData = {}
+  formData.first_name = $('#first_name').val()
+  formData.last_name = $('#last_name').val()
+  formData.password = $('#password').val()
+  formData.email = $('#email').val()
+
+  console.log('Form Data Submitted: ', formData)
+}
+/* after the page load will sign and call the function 
+which is 
+materialbox initializing the material box with jQuery
+add listener to button submit event click
+submitForm function will call when clicking on the submit button 
+addCards will add cards to index html page by a append them to the end of
+card-section div.
+model() function will initialize  modal with jQuery 
+*/
 
 $(document).ready(function () {
   $('.materialboxed').materialbox()
   $('#formSubmit').click(() => {
     submitForm()
   })
-  addCards(cardList)
+  addCards(catList)
   $('.modal').modal()
 })
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express')
 var app = express()
-
+// call index html when the server run
 app.use(express.static(__dirname + '/public'))
+//use json data format
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
+// run on port 3000
 var port = process.env.port || 3000
-
+// listen to port 3000
 app.listen(port, () => {
   console.log('App listening to: ' + port)
 })
